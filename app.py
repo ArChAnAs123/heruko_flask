1f7e946a5b8b4fa2661d01c93206bf6512836ee3
ArChAnAs123 <62280590+ArChAnAs123@users.noreply.github.com>
2021-05-30 17:58:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+
+
+port = int(os.environ.get("PORT", 5000))
 
 import flask
 import pickle
@@ -33,4 +37,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=port)
